98f71a8cced3b42f2c3bc0e38417f2c5622b0aee
Emma <emma.sioland@gmail.com>
2021-12-22 19:22:09 +0100

    done?

--- script.js
+++ script.js
@@ -1,101 +1,96 @@
+// Basic settings
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const particlesArray = [];
+const bubbleArray = []; // Define empty array for holding bubbles
+const colourPalette = [ // Define array for holding colours for bubbles
+    'rgb(45, 226, 230)', // Cyan
+    'rgb(253, 29, 83)',  // Coral
+    'rgb(247, 6, 207)',  // Neon pink
+    'rgb(146, 0, 117)']; // Plum
 
-// window.addEventListener('resize', function(){
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-    
-//     function animate(){
-//         ctx.clearRect(0,0,canvas.width, canvas.height);
-//         // drawCircle();
-//         handleParticles();
-//         requestAnimationFrame(animate);
-//     }
-    
-//     animate();
-// });
-
+// Define mouse coordinates
 const mouse = {
     x: undefined,
     y: undefined,
-}
+};
 
-canvas.addEventListener('click', function(event){
+// Tracks canvas resizing and adjusts accordingly
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+});
+
+// Track mouse clicks and bursts bubbles at mouse position
+canvas.addEventListener('click', function (event){
     mouse.x = event.x;
     mouse.y = event.y;
-    // drawCircle();
-    for(let i =0; i < 10; i++){
-        particlesArray.push(new Particle());
+    for(let i = 0; i < 10; i++){
+        bubbleArray.push(new Bubble());
     }
 });
 
-canvas.addEventListener('mousemove', function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
-    // drawCircle();
-//     for(let i =0; i < 5; i++){
-//         particlesArray.push(new Particle());
-//     }
-});
-
-// function drawCircle(){
-//     ctx.strokeStyle = 'red';
-//     ctx.beginPath();
-//     ctx.arc(mouse.x, mouse.y, 30, 0, Math.PI * 2);
-//     ctx.stroke();
-// }
-
-class Particle {
-    constructor() {
+class Bubble {
+    constructor(){
+        // Define bubbles initial position
         this.x = mouse.x;
         this.y = mouse.y;
-        // this.x = Math.random() * canvas.width;
-        // this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 30 + 1;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
+
+        this.size = Math.random() * 29 + 1; // Define a  random radius for bubbles
+        
+        // Defines number for random speed and random omnidirectional moving
+        this.speedX = Math.random() * 20 - 10;
+        this.speedY = Math.random() * 20 - 10;
+        
+        this.color =  colourPalette[Math.floor(Math.random() * colourPalette.length)] ; //Defines randomised colours for each bubble
     }
-    update() {
+    // Updates bubble size and position 
+    update(){
+        if (this.size>0.2) this.size -= 0.1; // Reduces size of bubble
+
+        // Makes speed number into vector 
         this.x += this.speedX;
         this.y += this.speedY;
-        if (this.size > 0.2) this.size -= 0.1;
     }
-    draw() {
-        ctx.strokeStyle = 'red';
+    // Draws bubble
+    draw(){
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 1.5;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.stroke();
     }
 }
 
-// function init(){
-//     for (let i = 0; i < 100; i++){
-//         particlesArray.push(new Particle());
-//     }
-// }
-// init();
+// Draws bubbles and updates in each index step
+function handlesBubbles(){
+    for (let i = 0; i < bubbleArray.length; i++){
+    bubbleArray[i].update();  // Updated bubble for index i 
+    bubbleArray[i].draw();  // Draws bubble for index i   
 
-function handleParticles(){
-    for (let i = 0; i < particlesArray.length; i++){
-        particlesArray[i].update();
-        particlesArray[i].draw();
-        if (particlesArray[i].size <= 0.3){
-            particlesArray.splice(i, 1);
-            i--;
+        // Makes bubbles bounce off sides
+        if (bubbleArray[i].y + bubbleArray[i].speedY < Bubble.size || bubbleArray[i].y + bubbleArray[i].speedY > canvas.height-Bubble.size){
+            bubbleArray[i].speedY = -bubbleArray[i].speedY;
+        }
+        if (bubbleArray[i].x + bubbleArray[i].speedX < Bubble.size || bubbleArray[i].x + bubbleArray[i].speedX > canvas.width-Bubble.size){
+            bubbleArray[i].speedX = -bubbleArray[i].speedX;
+        }
+
+        // Removes bubbles if very small
+        if (bubbleArray[i].size <= 0.2){    
+            bubbleArray.splice(i, 1);
+            i--; // Resets index after removal
         }
     }
-} 
+};
 
+// Animates the bubbles
 function animate(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
-    // ctx.fillStyle = 'rgba(0, 0, 0, 0.01)';
-    // ctx.fillRect(0,0,canvas.width, canvas.height);
-    // drawCircle();
-    handleParticles();
+    handlesBubbles();
     requestAnimationFrame(animate);
-}
+};
 
 animate();
